491f7a718b0901f2a03cc11098a9ccc0d6555ae2
Mansi Parikh <144649215+mansi2197@users.noreply.github.com>
2025-07-12 11:49:34 +0530

    Fixed empty java files - added actual code

--- stackit/src/main/java/com/stackit/stackit/entity/User.java
+++ stackit/src/main/java/com/stackit/stackit/entity/User.java
@@ -1,4 +1,10 @@
 package com.stackit.stackit.entity;
 
 public class User {
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String role;
 }
